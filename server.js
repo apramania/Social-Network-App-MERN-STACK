4a8d3f33802dec60d5382c83ae69737fee3a4ca2
Apratim Das <apratimdas18@gmail.com>
2020-07-30 16:14:16 +0530

    Prepared for deployment

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 const express = require('express')
 //bringing in the connection constant from db.js for mongodb(Database) connections
 const connectDB = require('./config/db')
+const path = require('path')
 
 //initialise the express module
 const app = express()
@@ -16,7 +17,7 @@ connectDB()
 app.use(express.json({ extended: false }))
 
 //setting up a basic route and sending resonse back to test if the server is working
-app.get('/', (req, res) => res.send("API running!"))
+// app.get('/', (req, res) => res.send("API running!"))
 
 //Defining Routes
 
@@ -24,6 +25,16 @@ app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/posts'))
+
+//serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+    //Set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 
 //setting up the port for the server to start
